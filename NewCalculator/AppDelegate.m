e82ebc38966dc06af9ed6cfa404513b29fe5ebcb
Anil Siruvolu <siruvolu@icloud.com>
2015-09-08 10:48:44 -0400

    Added - "Modified" on every classes

--- NewCalculator/AppDelegate.m
+++ NewCalculator/AppDelegate.m
@@ -5,6 +5,7 @@
 //  Created by Diego Leal Togni on 10/3/14.
 //  Copyright (c) 2014 Gannon University. All rights reserved.
 // iOS 6 Omer Abufara / sai anil siruvolu / Mohamed Hassan
+// modified by IOS6
 
 #import "AppDelegate.h"
 
