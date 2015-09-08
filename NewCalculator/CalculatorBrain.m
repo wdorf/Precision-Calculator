fdcaf620a138015c8f3c945fb1ab12c49d5c5ad1
Moran <moran014@macpc4.local>
2015-09-08 10:28:34 -0400

    Added comment "modified by iOS5"

--- NewCalculator/CalculatorBrain.m
+++ NewCalculator/CalculatorBrain.m
@@ -4,7 +4,7 @@
 //
 //  Created by Diego Leal Togni on 10/3/14.
 //  Copyright (c) 2014 Gannon University. All rights reserved.
-//  modified by KW
+//  modified by iOS5
 
 #import "CalculatorBrain.h"
 
