d743aea0ef4eb1b64580e5fbaadd83c93b7618ed
Alotaibi <alotaibi026@macpc12.gannon.edu>
2015-09-11 12:47:41 -0400

    Yellow

--- NewCalculator/AppDelegate.h
+++ NewCalculator/AppDelegate.h
@@ -4,7 +4,8 @@
 //
 //  Created by Diego Leal Togni on 10/3/14.
 //  Copyright (c) 2014 Gannon University. All rights reserved.
-//modified by scm_1
+//  Team iOS 4 members: Waleed Alotaibi, Srinivas, Abhinav
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
