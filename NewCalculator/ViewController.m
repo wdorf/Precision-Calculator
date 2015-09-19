e8ed737e4ca31da1c6ee09eb2f4cac60ab6f61a2
Alotaibi <alotaibi026@macpc12.gannon.edu>
2015-09-19 15:03:13 -0400

    Create Branch

--- NewCalculator/ViewController.m
+++ NewCalculator/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Diego Leal Togni on 10/3/14.
 //  Copyright (c) 2014 Gannon University. All rights reserved.
-//
+//  Fixing Smell 4
 
 #import "ViewController.h"
 #import "CalculatorBrain.h"
