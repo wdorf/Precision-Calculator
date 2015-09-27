53f770af3208087dd3a0f475e01dd4198bc1e3d8
Marco Orlando <morlando016@gmail.com>
2015-09-27 15:46:10 -0400

    Committing the change to the origin. I changed a variable name from num to newNum and added a test for precision.

--- NewCalculator/CalculatorBrain.m
+++ NewCalculator/CalculatorBrain.m
@@ -2,7 +2,7 @@
 //  CalculatorBrain.m
 //  NewCalculator
 //
-//  Created by Diego Leal Togni on 10/3/14.
+//  Created by Diego Leal Togni on 10/3/14..
 //  Copyright (c) 2014 Gannon University. All rights reserved.
 //  modified by KW
 
