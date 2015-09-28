7f61bf99f83499bf12b57d804f5947a43c162286
Moradiya, Bhavik K <moradiya001@macpc13.local>
2015-09-27 21:17:42 -0400

    Testing completed by FixingSmellTeam 4

--- NewCalculatorUITests/NewCalculatorUITests.m
+++ NewCalculatorUITests/NewCalculatorUITests.m
@@ -31,7 +31,223 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
-
+-(void)testAddition{
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"6"] tap];
+    [app.buttons[@"+"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"9"];
+    XCTAssertTrue(display.exists);
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"+"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    display= app.staticTexts[@"69"];
+    XCTAssertTrue(display.exists);
+    
+}
+-(void)testSubtraction{
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"6"] tap];
+    [app.buttons[@"-"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"3"];
+    XCTAssertTrue(display.exists);
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"-"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    display= app.staticTexts[@"63"];
+    XCTAssertTrue(display.exists);
+}
+-(void)testMultiplucation{
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"6"] tap];
+    [app.buttons[@"*"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"18"];
+    XCTAssertTrue(display.exists);
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"*"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    display= app.staticTexts[@"198"];
+    XCTAssertTrue(display.exists);}
+-(void)testDivision{
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"6"] tap];
+    [app.buttons[@"/"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"2"];
+    XCTAssertTrue(display.exists);
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"/"] tap];
+    [app.buttons[@"3"] tap];
+    [app.buttons[@"="] tap];
+    display= app.staticTexts[@"22"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testEPrecisionSub {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:0] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"-"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"0.33"];
+    display= app.staticTexts[@"0.33"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testEPrecisionAdd {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:0] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"+"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"4.99"];
+    display= app.staticTexts[@"4.99"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testEPrecisionMul {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:0] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"*"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"6.20"];
+    display= app.staticTexts[@"6.20"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testEPrecisionDiv {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:0] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"/"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"1.14"];
+    display= app.staticTexts[@"1.14"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testDaynicPrecisionAdd {
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:1] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"+"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"4.99"];
+    display= app.staticTexts[@"4.99"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testDaynicPrecisionSub {
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:1] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"-"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"0.33"];
+    display= app.staticTexts[@"0.33"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testDaynicPrecisionMul {
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:1] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"*"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"6.1978"];
+    display= app.staticTexts[@"6.1978"];
+    XCTAssertTrue(display.exists);
+}
+- (void)testDaynicPrecisionDiv {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [[[[[app.otherElements containingType:XCUIElementTypeStaticText identifier:@"0"] childrenMatchingType:XCUIElementTypeButton] matchingIdentifier:@"Teste"] elementBoundByIndex:1] tap];
+    
+    XCUIElement *button = app.buttons[@"2"];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"."];
+    [button2 tap];
+    [app.buttons[@"6"] doubleTap];
+    [app.buttons[@"/"] tap];
+    [button tap];
+    [button2 tap];
+    [app.buttons[@"3"] doubleTap];
+    [app.buttons[@"="] tap];
+    XCUIElement *display= app.staticTexts[@"1.14163"];
+    display= app.staticTexts[@"1.14163"];
+    XCTAssertTrue(display.exists);
+}
 - (void)testSin
 {
     
