a9dfe6fce192b267575423d263f13a66cafd8d3c
Sophia Rodriguez <sophiarodriguez@sophias-macbook-pro.local>
2015-09-29 14:45:10 -0400

    Addition and subtraction test cases implemented

--- NewCalculatorUITests/NewCalculatorUITests.m
+++ NewCalculatorUITests/NewCalculatorUITests.m
@@ -32,9 +32,107 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+/*- (void)testAddition{
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"6"] tap];
+    
+    XCUIElement *button = app.buttons[@"+"];
+    [button tap];
+    [app.buttons[@"2"] tap];
+    
+    XCUIElement *button2 = app.buttons[@"="];
+    [button2 tap];
+    
+    //assert 1
+    XCUIElement *display = app.staticTexts[@"8"];
+    XCTAssertTrue(display.exists);
+    
+    
+    XCUIElement *cButton = app.buttons[@"C"];
+    [cButton tap];
+    
+    XCUIElement *button3 = app.buttons[@"1"];
+    [button3 tap];
+    
+    XCUIElement *button4 = app.buttons[@"0"];
+    [button4 tap];
+    
+    XCUIElement *button5 = app.buttons[@"+/-"];
+    [button5 tap];
+    [button tap];
+    [app.buttons[@"3"] tap];
+    [button2 tap];
+    
+    //assert 2
+    display = app.staticTexts[@"-7"];
+    XCTAssertTrue(display.exists);
+    
+    [cButton tap];
+    [button3 tap];
+    [button4 tap];
+    [button5 tap];
+    [button tap];
+    [button3 tap];
+    [app.buttons[@"5"] tap];
+    [button2 tap];
+    //assert 3
+    display = app.staticTexts[@"5"];
+    XCTAssertTrue(display.exists);
+}*/
+
+-(void)testSubtraction{
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"9"] tap];
+    
+    XCUIElement *button = app.buttons[@"."];
+    [button tap];
+    
+    XCUIElement *button2 = app.buttons[@"5"];
+    [button2 tap];
+    
+    XCUIElement *button3 = app.buttons[@"-"];
+    [button3 tap];
+    [app.buttons[@"4"] tap];
+    [button tap];
+    [button2 tap];
+    
+    XCUIElement *button5 = app.buttons[@"="];
+    [button5 tap];
+    //assert 1
+    XCUIElement *display = app.staticTexts[@"5"];
+    XCTAssertTrue(display.exists);
+    
+    XCUIElement *cButton = app.buttons[@"C"];
+    [cButton tap];
+    
+    XCUIElement *button6 = app.buttons[@"2"];
+    [button6 tap];
+    
+    XCUIElement *button4 = app.buttons[@"1"];
+    [button4 tap];
+    [app.buttons[@"+/-"] tap];
+    [button3 tap];
+    [button4 tap];
+    
+    XCUIElement *button7 = app.buttons[@"0"];
+    [button7 tap];
+    [button5 tap];
+    //assert 2
+    display = app.staticTexts[@"-31"];
+    XCTAssertTrue(display.exists);
+    
+    [cButton tap];
+    [button4 tap];
+    [app.buttons[@"7"] tap];
+    [button3 tap];
+    [button6 tap];
+    [button7 tap];
+    [button5 tap];
+    //assert 3
+    display = app.staticTexts[@"-3"];
+    XCTAssertTrue(display.exists);
 }
 
 @end
